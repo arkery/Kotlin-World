4bbdc1ebe5b9354a76f7f6e899c6e41fbc60c543
arkery <senseiark@gmail.com>
2020-02-08 22:21:37 -0800

    Figured out package issue & maven for Kotlin

--- KotlinLearningThreadsAndCoroutines/src/main/java/Intro.kt
+++
@@ -1,9 +0,0 @@
-
-fun main(){
-
-    println("Using maven with kotlin is a little bit odd since at least " +
-    "for intellij, you have to create it via java first")
-
-
-
-}
---
+++ LearningKotlinConcurrency/src/main/kotlin/io/github/arkery/Hello.kt
@@ -0,0 +1,6 @@
+package io.github.arkery
+
+fun main(args: Array<String>) {
+    println("Hello, World")
+}
+
