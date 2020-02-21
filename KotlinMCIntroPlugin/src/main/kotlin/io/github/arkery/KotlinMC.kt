d41522259f415cdcd09457cc8264f8703b0e31ac
arkery <senseiark@gmail.com>
2020-02-20 21:51:15 -0800

    Minor Sqlite

--- KotlinMCIntroPlugin/src/main/kotlin/io/github/arkery/KotlinMC.kt
+++ KotlinMCIntroPlugin/src/main/kotlin/io/github/arkery/KotlinMC.kt
@@ -1,20 +1,49 @@
 package io.github.arkery
 
+import org.bukkit.Bukkit
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import org.bukkit.event.Listener
 import org.bukkit.plugin.java.JavaPlugin
+import java.io.File
+import java.io.IOException
+import java.sql.Connection
+import java.sql.DriverManager
 
 class KotlinMC: JavaPlugin(), Listener, CommandExecutor{
 
+    private val dbLocation: File = File(this.dataFolder, "test.db")
+    private var connection: Connection? = null
+
+
     override fun onEnable() {
-        println("Hello World")
+        println("Starting KotlinMC Plugin")
+
+        try{
+
+            if(!dbLocation.exists()){
+                println("Attempting to create database")
+                this.dataFolder.mkdir()
+                this.dbLocation.createNewFile()
+            }
+
+            Class.forName("org.sqlite.JDBC")
+            this.connection = DriverManager.getConnection("jdbc:sqlite:${this.dbLocation}")
+            println("Created database at: ${dbLocation.absolutePath}")
+        }catch (f: ClassNotFoundException){
+            println("Missing JDBC Driver")
+        }catch (g: IOException){
+            println("Unable to create Folder")
+        }
+
+        Bukkit.getPluginManager().registerEvents(this, this)
     }
 
     override fun onDisable() {
         println("Closing the Plugin")
+        this.connection?.close()
     }
 
     override fun onCommand(sender: CommandSender, command: Command, label: String, args: Array<out String>): Boolean {
